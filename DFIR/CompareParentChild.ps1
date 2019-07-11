9710e39fc623dd2670718c16681eb03b13736980
Grzegorz Tworek <gtworek@users.noreply.github.com>
2019-07-11 22:03:10 +0200

    Add files via upload

---
+++ DFIR/CompareParentChild.ps1
@@ -0,0 +1,20 @@
+﻿####### simple routine allowing you to compare your freshly collected data to the data you have from a machine you know as healthy
+
+$oldfile = "wellknownparentchild.txt"
+$newfile = "newparentchild.txt"
+
+$knownpairs = Get-Content $oldfile
+$newpairs = Get-Content $newfile
+
+$sarr2=@()
+$sarr2 += '"Parent","CmdLine"'
+foreach ($pair in $newpairs)
+    {
+    if (-not $knownpairs.Contains($pair))
+        {
+        $sarr2+=$pair
+        }
+    }
+
+$sarr2 = ($sarr2 | ConvertFrom-Csv)
+
