84850efdafcedbae7bc3f5cc9b2b758cedae9984
Grzegorz Tworek <gtworek@users.noreply.github.com>
2019-07-11 12:11:59 +0200

    Add files via upload

---
+++ Misc/OpenVPNDecrypt.ps1
@@ -0,0 +1,17 @@
+﻿$keys = Get-ChildItem "HKCU:\Software\OpenVPN-GUI\configs"
+$items = $keys | ForEach-Object {Get-ItemProperty $_.PsPath}
+
+foreach ($item in $items)
+{
+    $encryptedbytes=$item.'auth-data'
+    $entropy=$item.'entropy'
+    $entropy=$entropy[0..(($entropy.Length)-2)]
+
+    $decryptedbytes = [System.Security.Cryptography.ProtectedData]::Unprotect(
+        $encryptedBytes, 
+        $entropy, 
+        [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+ 
+    Write-Host ($item.PSChildName), ([System.Text.Encoding]::Unicode.GetString($item.username)), ([System.Text.Encoding]::Unicode.GetString($decryptedbytes))
+}
+
