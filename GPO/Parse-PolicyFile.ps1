69bb8a38d1da76c4cfec1f10d85d92ab8d7f51e7
Grzegorz Tworek <gtworek@users.noreply.github.com>
2019-09-25 23:11:45 +0200

    Update Parse-PolicyFile.ps1

--- GPO/Parse-PolicyFile.ps1
+++ GPO/Parse-PolicyFile.ps1
@@ -10,33 +10,47 @@ $arrtmp2=@()
 
 if (Test-Path ($file2.FullName))
     {
-    $bytes = [System.IO.File]::ReadAllBytes($file2.FullName)
-    $text = [System.Text.Encoding]::ASCII.GetString($bytes, 0, 4)
-    $row = New-Object psobject
-    $row | Add-Member -Name FullName -MemberType NoteProperty -Value $file2.FullName
+    $polbytes = [System.IO.File]::ReadAllBytes($file2.FullName)
+    $text = [System.Text.Encoding]::ASCII.GetString($polbytes, 0, 4)
     if ($text -cne "PReg")
         {
         Write-Host ("[???] Not looking like POL file "+$file2.FullName) -ForegroundColor Red
-        $row | Add-Member -Name "Root" -MemberType NoteProperty -Value "???"
-        $row | Add-Member -Name "Key" -MemberType NoteProperty -Value "???"
-        $row | Add-Member -Name "Value" -MemberType NoteProperty -Value "???"
-        $row | Add-Member -Name "Data" -MemberType NoteProperty -Value "???"
+        $polrow = New-Object psobject
+        $polrow | Add-Member -Name "FullName" -MemberType NoteProperty -Value $file2.FullName
+        $polrow | Add-Member -Name "Root" -MemberType NoteProperty -Value "???"
+        $polrow | Add-Member -Name "Key" -MemberType NoteProperty -Value "???"
+        $polrow | Add-Member -Name "Value" -MemberType NoteProperty -Value "???"
+        $polrow | Add-Member -Name "Data" -MemberType NoteProperty -Value "???"
         }
     else
         {
-        #Let's extract root, key, value and data. Type and size to be ignored as not very interesting. Root (HKLM/HKCU) to be determined from name, ? for unknown
+        #Root (HKLM/HKCU) to be determined from name, ? for unknown, stupid but works
+        $POLroot = "?"
+        if ($file2.FullName -like "*}_Machine_Registry.pol")
+            {
+            $POLroot = "HKLM"
+            }
+        if ($file2.FullName -like "*}_User_Registry.pol")
+            {
+            $POLroot = "HKCU"
+            }
+        #[key;value;type;size;data] - let's extract key, value and data. Type and size to be ignored as not very interesting. 
         #assuming all pol files are unicode. let me know if you find different one.
-        $bytes = $bytes[8..($bytes.Count)]
-
-
-        $row | Add-Member -Name "Root" -MemberType NoteProperty -Value "root"
-        $row | Add-Member -Name "Key" -MemberType NoteProperty -Value "key"
-        $row | Add-Member -Name "Value" -MemberType NoteProperty -Value "value"
-        $row | Add-Member -Name "Data" -MemberType NoteProperty -Value "data"
+        $polbytes = $polbytes[8..($polbytes.Count)]
+        $PolStrings = [System.Text.Encoding]::Unicode.GetString($polbytes)
+        $PolStrings = $PolStrings[0..($PolStrings.Length-3)]
+        foreach ($polstrtmp in ($PolStrings -split "\["))
+            {
+            $polstrtmp2 = $polstrtmp -split ";"
+            $polrow = New-Object psobject
+            $polrow | Add-Member -Name "FullName" -MemberType NoteProperty -Value $file2.FullName
+            $polrow | Add-Member -Name "Root" -MemberType NoteProperty -Value $POLroot
+            $polrow | Add-Member -Name "Key" -MemberType NoteProperty -Value $polstrtmp2[0]
+            $polrow | Add-Member -Name "Value" -MemberType NoteProperty -Value $polstrtmp2[1]
+            $polrow | Add-Member -Name "Data" -MemberType NoteProperty -Value $polstrtmp2[4]
+            $arrtmp2 += $polrow
+            }
         }
-
-    $arrtmp2 += $row
-
     if ($file -eq $null)
         {
         $arrtmp2 | Out-GridView
