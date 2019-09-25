d25f00d8f30102df49ad9d2edae6a72db36fbeef
Grzegorz Tworek <gtworek@users.noreply.github.com>
2019-09-25 10:20:35 +0200

    Update Parse-PolicyFile.ps1

--- GPO/Parse-PolicyFile.ps1
+++ GPO/Parse-PolicyFile.ps1
@@ -7,23 +7,31 @@ else
     $file2 = $file
     }
 $arrtmp2=@()
-$bytes = [System.IO.File]::ReadAllBytes($file2.FullName)
-$text = [System.Text.Encoding]::ASCII.GetString($bytes, 0, 4)
-$row = New-Object psobject
-$row | Add-Member -Name FullName -MemberType NoteProperty -Value $file2.FullName
-if ($text -cne "PReg")
+
+if (Test-Path ($file2.FullName))
     {
-    Write-Host ("[???] Not looking like POL file "+$file2.FullName) -ForegroundColor Red
-    $row | Add-Member -Name "Format" -MemberType NoteProperty -Value "Invalid"
-    }
+    $bytes = [System.IO.File]::ReadAllBytes($file2.FullName)
+    $text = [System.Text.Encoding]::ASCII.GetString($bytes, 0, 4)
+    $row = New-Object psobject
+    $row | Add-Member -Name FullName -MemberType NoteProperty -Value $file2.FullName
+    if ($text -cne "PReg")
+        {
+        Write-Host ("[???] Not looking like POL file "+$file2.FullName) -ForegroundColor Red
+        $row | Add-Member -Name "Format" -MemberType NoteProperty -Value "Invalid"
+        }
+    else
+        {
+        $row | Add-Member -Name "Format" -MemberType NoteProperty -Value "Valid"
+        }
+
+    $arrtmp2 += $row
+
+    if ($file -eq $null)
+        {
+        $arrtmp2 | Out-GridView
+        }
+    } #file exists
 else
     {
-    $row | Add-Member -Name "Format" -MemberType NoteProperty -Value "Valid"
-    }
-
-$arrtmp2 += $row
-
-if ($file -eq $null)
-    {
-    $arrtmp2 | Out-GridView
+    Write-Host ("[???] Cannot find the file for parsing") -ForegroundColor Red
     }
