af732c36d04d7779a27713e0ad1776e713268193
Grzegorz Tworek <gtworek@users.noreply.github.com>
2019-11-26 15:14:26 +0100

    Create Get-BitLockerPassword.ps1

---
+++ Misc/Get-BitLockerPassword.ps1
@@ -0,0 +1,19 @@
+# 1. Feel free to wrap it into a nice function and return data as PSObject if you like.
+# 2. Of course you can initialize your $computer variable other ways, i.e. with Get-Content
+# 3. Computers must be reachable through WMI
+# 4. WMI errors are muted with -ErrorAction. Feel free to change this if you like red fonts.
+# 5. BitLocker WMI provider class is documented at https://docs.microsoft.com/en-us/windows/win32/secprov/win32-encryptablevolume
+
+$computers=@("localhost","someotherhost")
+
+foreach ($computername in $computers)
+{
+    foreach ($disk in (Get-WmiObject -ComputerName $computername -namespace "Root\cimv2\security\MicrosoftVolumeEncryption" -ClassName "Win32_Encryptablevolume" -ErrorAction SilentlyContinue))
+    {
+        foreach ($protectorid in ($disk.GetKeyProtectors("3").volumekeyprotectorID))
+        {
+            write-host $computername,`t,$disk.DriveLetter,`t,$protectorid,`t,($disk.GetKeyProtectorNumericalPassword($protectorid)).NumericalPassword
+        }
+
+    }
+}
