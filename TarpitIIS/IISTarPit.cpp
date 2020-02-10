5d2d2b722e55253fd59af0dd1435c7eb2f9bb610
Grzegorz Tworek <gtworek@users.noreply.github.com>
2020-02-10 13:32:22 +0100

    pattern and the configurable delay

--- TarpitIIS/IISTarPit.cpp
+++ TarpitIIS/IISTarPit.cpp
@@ -5,6 +5,10 @@
 #include <windows.h>
 #include <httpserv.h>
 
+const DWORD gdefaultDelay = 500; // ms of delay to be applied if no registry value can be read. 
+const CHAR gPattern[] = "test"; //pattern to check against. If present in the HTTP_URL, the delay happens
+
+DWORD gdwDelay;
 
 // Module class
 class MyHttpModule : public CHttpModule
@@ -18,9 +22,32 @@ public:
 			IN IHttpEventProvider* pProvider
 		)
 	{
-		UNREFERENCED_PARAMETER(pHttpContext);
 		UNREFERENCED_PARAMETER(pProvider);
-		Sleep(1000);
+
+		const DWORD dwMaxBufSize = 1024; //maximum url length (buf size) to protect from DoS attacks
+
+		DWORD dwBufLen = dwMaxBufSize;
+		CHAR szBuf[dwMaxBufSize];
+		PCSTR szVariableName = "HTTP_URL";
+		PCSTR pszBuf = szBuf;
+		BOOL bSleep = FALSE;
+
+		if (FAILED(pHttpContext->GetServerVariable(szVariableName, &pszBuf, &dwBufLen)))
+		{
+			bSleep = TRUE; //failed, let's sleep just in case. 
+		}
+		else
+		{
+			if (strstr(pszBuf, gPattern) != NULL)
+			{
+				bSleep = TRUE;
+			}
+		}
+
+		if (bSleep)
+		{
+			Sleep(gdwDelay);
+		}
 		return RQ_NOTIFICATION_CONTINUE;
 	}
 };
@@ -67,8 +94,8 @@ HRESULT
 __stdcall
 RegisterModule(
 	DWORD dwServerVersion,
-	IHttpModuleRegistrationInfo* pModuleInfo,
-	IHttpServer* pGlobalInfo
+	IHttpModuleRegistrationInfo * pModuleInfo,
+	IHttpServer * pGlobalInfo
 )
 {
 	UNREFERENCED_PARAMETER(dwServerVersion);
@@ -77,10 +104,50 @@ RegisterModule(
 	return pModuleInfo->SetRequestNotifications(
 		new MyHttpModuleFactory, // Class factory
 		RQ_BEGIN_REQUEST, // Event notifications
-		0 // Post-event notifications
-	);
+		0); // Post-event notifications
 }
 
+
+// returns the DWORD value stored in hklm\SOFTWARE\IISTarPit\delay 
+DWORD regReadDelay()
+{
+	HKEY hKey;
+	LSTATUS status;
+	DWORD dwDelay;
+	DWORD cbData = sizeof(DWORD);
+	DWORD dwRegType = REG_DWORD;
+
+	status = RegOpenKeyEx(
+		HKEY_LOCAL_MACHINE,
+		TEXT("SOFTWARE\\IISTarPit"),
+		NULL,
+		KEY_QUERY_VALUE,
+		&hKey
+	);
+
+	if (status != ERROR_SUCCESS)
+	{
+		return gdefaultDelay;
+	}
+
+	status = RegQueryValueEx(
+		hKey,
+		TEXT("delay"),
+		NULL,
+		&dwRegType,
+		(LPBYTE)&dwDelay,
+		&cbData
+	);
+
+	RegCloseKey(hKey);
+
+	if (status != ERROR_SUCCESS)
+	{
+		return gdefaultDelay;
+	}
+
+	return dwDelay;
+}
 
 __declspec(dllexport)
 BOOL
@@ -91,6 +158,12 @@ DllMain(
 	LPVOID lpReserved
 )
 {
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			gdwDelay = regReadDelay();
+			break;
+	}
 	return TRUE;
 }
 
