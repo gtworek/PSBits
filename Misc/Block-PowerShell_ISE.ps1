9a9f39c015cb1003a54d13203007f37fd932b9e4
Grzegorz Tworek <grzegorz.tworek@stderr.pl>
2020-07-08 12:58:15 +0200

    Comment added

--- Misc/Block-PowerShell_ISE.ps1
+++ Misc/Block-PowerShell_ISE.ps1
@@ -1,3 +1,6 @@
+# Very simple script creating a mutex.
+# As powershell_ise waits for such mutex, it effectively block new ISE instances from displaying its window.
+
 $mtx = New-Object System.Threading.Mutex($false, "GPowerShellInitializationMutexName")
 [void]$mtx.WaitOne()
 
