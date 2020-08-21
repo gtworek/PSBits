6d2614ad295baec11572cacc69a143be2786ff2e
Grzegorz Tworek <gtworek@users.noreply.github.com>
2020-08-21 21:16:47 +0200

    ASCII -> UTF8 (thanks @NestoriSyynimaa)

--- Misc/DumpChromePasswords.ps1
+++ Misc/DumpChromePasswords.ps1
@@ -41,7 +41,7 @@ foreach ($datarow in $data.Tables.rows)
     $row = New-Object psobject
     $row | Add-Member -Name URL -MemberType NoteProperty -Value ($datarow.origin_url)
     $row | Add-Member -Name UserName -MemberType NoteProperty -Value ($datarow.username_value)
-    $row | Add-Member -Name Password -MemberType NoteProperty -Value (([System.Text.Encoding]::ASCII.GetString([System.Security.Cryptography.ProtectedData]::Unprotect($datarow.password_value,$null,[System.Security.Cryptography.DataProtectionScope]::CurrentUser))))
+    $row | Add-Member -Name Password -MemberType NoteProperty -Value (([System.Text.Encoding]::UTF8.GetString([System.Security.Cryptography.ProtectedData]::Unprotect($datarow.password_value,$null,[System.Security.Cryptography.DataProtectionScope]::CurrentUser))))
     $arrExp += $row
 }
 
