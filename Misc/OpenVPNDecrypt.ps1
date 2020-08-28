e81f3e8d220de2bf377c860038ad1a6dac6a613e
Grzegorz Tworek <gtworek@users.noreply.github.com>
2020-08-28 14:51:52 +0200

    Add-Type, following haim-n suggestion

--- Misc/OpenVPNDecrypt.ps1
+++ Misc/OpenVPNDecrypt.ps1
@@ -1,4 +1,5 @@
-﻿$keys = Get-ChildItem "HKCU:\Software\OpenVPN-GUI\configs"
+Add-Type -AssemblyName System.Security
+$keys = Get-ChildItem "HKCU:\Software\OpenVPN-GUI\configs"
 $items = $keys | ForEach-Object {Get-ItemProperty $_.PsPath}
 
 foreach ($item in $items)
