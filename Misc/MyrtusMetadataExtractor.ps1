69bd6a18aa5030fdf59aa2f47fa3d94723c6adb5
Grzegorz Tworek <gtworek@users.noreply.github.com>
2020-09-05 11:32:50 +0200

    Create MyrtusMetadataExtractor.ps1

---
+++ Misc/MyrtusMetadataExtractor.ps1
@@ -0,0 +1,66 @@
+# simple meteadata extractor for the repository published by @Myrtus0x0 at https://github.com/myrtus0x0/Pastebin-Scraping-Results/tree/master/base64MZHeader
+
+$workingdir = "C:\Temp\Myrtus"
+
+$arrExp=@()
+foreach ($file in (Get-ChildItem -Path $workingdir))
+{
+    $filename = $file.FullName
+    $row = New-Object psobject
+    $row | Add-Member -Name "Name" -MemberType NoteProperty -Value (Split-Path $filename -leaf)
+    $signature = Get-AuthenticodeSignature -FilePath $filename
+    $certSubject = ""
+    if ($signature.Status.value__ -eq 0)  #valid
+    {
+        $certSubject = $signature.SignerCertificate.Subject
+    }
+    $row | Add-Member -Name "Signer" -MemberType NoteProperty -Value $certSubject
+    $row | Add-Member -Name "Description" -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.FileDescription
+    $row | Add-Member -Name "InternalName"  -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.InternalName
+    $row | Add-Member -Name "OriginalFilename"  -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.OriginalFilename
+    $row | Add-Member -Name "ProductName"  -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.ProductName
+    $row | Add-Member -Name "VersionInfo"  -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.VersionInfo
+    $row | Add-Member -Name "Comments"      -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.Comments
+    $row | Add-Member -Name "CompanyName"  -MemberType NoteProperty -Value (Get-Command $filename).FileVersionInfo.CompanyName
+    $row | Add-Member -Name "MD5"  -MemberType NoteProperty -Value (Get-FileHash -Path $filename -Algorithm MD5).Hash
+        $arrExp += $row
+}
+
+# Let's display the array
+if (Test-Path Variable:PSise)
+{
+    $arrExp | Out-GridView
+}
+else
+{
+    $arrExp | Format-List
+}
+
+
+# rough (hardcoded columns) but working convert to markdown. 
+$markdown = ""
+$markdown += "Name | Signer | Descritption | InternalName | OriginalFilename | ProductName | VersionInfo | Comments | CompanyName | MD5`r`n"
+$markdown += " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- `r`n"
+foreach ($row in $arrExp)
+{
+    $markdown += $row.Name
+    $markdown += "|"
+    $markdown += $row.Signer
+    $markdown += "|"
+    $markdown += $row.Descritption
+    $markdown += "|"
+    $markdown += $row.InternalName
+    $markdown += "|"
+    $markdown += $row.OriginalFilename
+    $markdown += "|"
+    $markdown += $row.ProductName
+    $markdown += "|"
+    $markdown += $row.VersionInfo
+    $markdown += "|"
+    $markdown += $row.Comments
+    $markdown += "|"
+    $markdown += $row.CompanyName
+    $markdown += "|"
+    $markdown += $row.MD5
+    $markdown += "`r`n"
+}
