f186f7a767c46a119b8a50ffcd1dd08ba1a2ae3f
Grzegorz Tworek <gtworek@users.noreply.github.com>
2021-01-15 21:50:26 +0100

    Add files via upload

---
+++ CERTPL2Hosts/Install-CERTHosts.ps1
@@ -0,0 +1,90 @@
+﻿# Skrypt instalujący 
+# 1. Sprawdza czy aby pliku nie ma już w program files. A nuż ktoś woli go tam ręcznie wrzucić.
+# 2. Pobiera aktualny plik skryptu z repozytorium, jeżeli go nie ma w pkt 1.
+# 3. Tworzy Scheduled Task, uruchamiający skrypt raz na godzinę. 
+#
+# Instalacja wymaga praw admina, bo task musi być uruchamiany z wysokimi przywilejami, 
+# żeby móc modyfikować plik hosts.
+#
+# W trakcie działania, nowe definicje pobierane będą tylko z CERT.PL.
+
+# czy admin?
+if (!([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")))
+{
+    Write-Host "BŁĄD: Skrypt wymaga uprawnień administratora." -ForegroundColor Red -BackgroundColor Yellow
+    break
+}
+
+# Parametry działania
+$ScriptFolder = ($env:ProgramFiles)+"\"
+$ScriptName = "Update-CERTHosts.ps1"
+$TaskName = "CERT.PL do hosts"
+$ScriptURL = "https://raw.githubusercontent.com/gtworek/PSBits/master/CERTPL2hosts/Update-CERThosts.ps1"
+
+# Sprawdzamy czy już może jest plik
+if (!(Test-Path -LiteralPath ($ScriptFolder+$ScriptName)))
+{
+    
+    # Pobierz skrypt z githuba
+    $WebRequest = Invoke-WebRequest -Uri $ScriptURL -UseBasicParsing -OutFile ($ScriptFolder+$ScriptName)
+    # Sprawdź czy się pobrała poprawnie
+    if ($WebRequest.StatusCode -ne 200) 
+    {
+        Write-Host "BŁĄD: Pobranie pliku nieudane." -ForegroundColor Red -BackgroundColor Yellow
+        break
+    }
+}
+
+# na wszelki wypadek sprawdzamy czy nie ma już taska.
+$Task = Get-ScheduledTask -TaskName $TaskName -ErrorAction Ignore
+if ($Task -ne $null)
+{
+    # Koniec, nie robimy nowego! 
+    Write-Host "BŁĄD: Scheduled Task już istnieje! Nowy NIE zostanie zarejestrowany." -ForegroundColor Red -BackgroundColor Yellow
+    break
+}
+
+# Krok #3
+$Service = new-object -ComObject ("Schedule.Service")
+$Service.Connect($Env:computername)
+$RootFolder = $Service.GetFolder("\")
+$TaskDefinition = $Service.NewTask(0) # W MSDN piszą, że musi być 0
+$TaskDefinition.RegistrationInfo.Description = $TaskNameKeeper
+$TaskDefinition.RegistrationInfo.Author = $Env:username
+$TaskDefinition.Settings.AllowDemandStart = $true
+$TaskDefinition.Settings.AllowHardTerminate = $true
+$TaskDefinition.Settings.DisallowStartIfOnBatteries = $false
+$TaskDefinition.Settings.Enabled = $true
+$TaskDefinition.Settings.ExecutionTimeLimit = "PT72H"
+$TaskDefinition.Settings.Hidden = $false
+$TaskDefinition.Settings.IdleSettings.RestartOnIdle = $false
+$TaskDefinition.Settings.IdleSettings.StopOnIdleEnd = $false
+$TaskDefinition.Settings.MultipleInstances = 0 # TASK_INSTANCES_PARALLEL
+$TaskDefinition.Settings.RunOnlyIfNetworkAvailable = $false 
+$TaskDefinition.Settings.StartWhenAvailable = $true
+$TaskDefinition.Settings.StopIfGoingOnBatteries = $false
+$TaskDefinition.Settings.WakeToRun = $false
+$Trigger = $TaskDefinition.Triggers.Create(2) # TASK_TRIGGER_DAILY
+$Trigger.Repetition.Interval = "PT1H" # raz na godzinę
+$Trigger.Repetition.Duration = "P1D"
+$Trigger.Repetition.StopAtDurationEnd = $false
+$Trigger.ExecutionTimeLimit = "P3D"
+$Trigger.StartBoundary = "2000-01-01T00:00:00" 
+$Trigger.DaysInterval = 1 
+$Trigger.Enabled = $true
+$Action = $TaskDefinition.Actions.Create(0) # TASK_ACTION_EXEC
+$Action.Path = (Get-Command powershell.exe).Definition
+$Action.Arguments = "-file """+($ScriptFolder+$ScriptName)+""""
+$Action.WorkingDirectory = $ScriptFolder
+$Task = $RootFolder.RegisterTaskDefinition($TaskName,$TaskDefinition,6,"SYSTEM",$null,5)  # TASK_CREATE_OR_UPDATE, TASK_LOGON_SERVICE_ACCOUNT
+
+# sprawdzamy czy na pewno task jest.
+$Task = Get-ScheduledTask -TaskName $TaskName -ErrorAction Ignore
+if ($Task -eq $null)
+{
+    # Nie ma, a powinien być. 
+    Write-Host "BŁĄD: Scheduled Task NIE został zarejestrowany." -ForegroundColor Red -BackgroundColor Yellow
+    break
+}
+
+Write-Host "Instalacja wykonana poprawnie." -ForegroundColor DarkGreen -BackgroundColor Yellow
---
+++ CERTPL2Hosts/Update-CERTHosts.ps1
@@ -0,0 +1,79 @@
+﻿
+$HoleCertURL = "http://hole.cert.pl/domains/domains_hosts.txt" # skąd pobieramy dane
+$HostsFile = $env:windir+"\System32\drivers\etc\hosts." # gdzie je wpisujemy
+$BackupFile = ($env:windir+"\System32\drivers\etc\hosts_holecert.bak") # backup
+$HoleCertStart = "### Start of "+$HoleCertURL+" content ###" # znacznik początku danych z CERT
+$HoleCertEnd = "### End of "+$HoleCertURL+" content ###" # znacznik końca danych z CERT
+$MutexName = "CERT2HostsMutex" # nazwa mutexa zabezpieczającego skrypt przed równoległym działaniem 
+
+# zróbmy mutex, póki nie go nie zwolnimy, to druga instancja będzie czekać
+$mtx = New-Object System.Threading.Mutex($false, $MutexName)
+[void]$mtx.WaitOne()
+
+# Backup. Wykonywany tylko raz, na wszelki wypadek.
+if (!(Test-Path -LiteralPath $BackupFile))
+{
+    Copy-Item -LiteralPath  ($env:windir+"\System32\drivers\etc\hosts.") -Destination $BackupFile
+}
+
+# Pobierz listę z CERT
+$HoleCertResponse = Invoke-WebRequest -Uri $HoleCertURL -UseBasicParsing
+
+# Sprawdź czy się pobrała poprawnie
+if ($HoleCertResponse.StatusCode -ne 200) 
+{
+    [void]$mtx.ReleaseMutex()
+    $mtx.Dispose()
+    exit  
+}
+
+# Wyciągnij z odpowiedzi linie do hosts
+$HoleCertContent = $HoleCertResponse.Content
+
+# Popraw LF z weba na CRLF dla pliku
+$HoleCertContent = $HoleCertContent.Replace("`n","`r`n")
+
+# Weź zawartość hosts
+$HostsFileContent = Get-Content -LiteralPath $HostsFile
+
+# Znajdź w pliku hosts początek i koniec sekcji z CERT. 
+$HoleCertStartLine = ($HostsFileContent | Select-String  -Pattern $HoleCertStart -SimpleMatch).LineNumber
+$HoleCertEndLine = ($HostsFileContent | Select-String  -Pattern $HoleCertEnd -SimpleMatch).LineNumber
+
+
+# Są 4 przypadki:
+# Przypadek 1: Mamy obie linie - wymieniamy zawartość między nimi.
+# Przypadek 2: Nie mamy żadnej - dodajemy na końcu pliku start, zawartość i end.
+# Przypadek 3: Mamy tylko start - zostawiamy początek, dodajemy zawartość i end i resztę pliku.
+# Przypadek 4: Mamy tylko end - wstawiamy start i zawartość przed end.
+
+# 1
+if (($HoleCertStartLine -ne $null) -and ($HoleCertEndLine -ne $null))
+{
+    $NewHostsContent = $HostsFileContent[0..($HoleCertStartLine-1)] + $HoleCertContent + $HostsFileContent[($HoleCertEndLine-1)..($HostsFileContent.Count-1)]
+}
+
+# 2
+if (($HoleCertStartLine -eq $null) -and ($HoleCertEndLine -eq $null))
+{
+    $NewHostsContent = $HostsFileContent + $HoleCertStart + $HoleCertContent + $HoleCertEnd
+}
+
+# 3
+if (($HoleCertStartLine -ne $null) -and ($HoleCertEndLine -eq $null))
+{
+    $NewHostsContent = $HostsFileContent[0..($HoleCertStartLine-1)] + $HoleCertContent + $HoleCertEnd + $HostsFileContent[($HoleCertStartLine)..($HostsFileContent.Count-1)]
+}
+
+# 4
+if (($HoleCertStartLine -eq $null) -and ($HoleCertEndLine -ne $null))
+{
+    $NewHostsContent = $HostsFileContent[0..($HoleCertEndLine-2)] + $HoleCertStart +  $HoleCertContent + $HostsFileContent[($HoleCertEndLine-1)..($HostsFileContent.Count-1)]
+}
+
+# nadpisujemy plik hosts
+$NewHostsContent | Out-File $HostsFile -Force
+
+# zwalniamy mutex
+[void]$mtx.ReleaseMutex()
+$mtx.Dispose()
