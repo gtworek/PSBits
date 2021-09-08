9fc2dadb4f15cd4501ac8712e9b6dcc2f80efcaa
Grzegorz Tworek <gtworek@users.noreply.github.com>
2021-09-08 14:27:50 +0200

    link added

--- Misc/Disable-ActiveX.ps1
+++ Misc/Disable-ActiveX.ps1
@@ -1,4 +1,6 @@
-#disable Activex - the workaround suggested by Microsoft for the CVE-2021-40444
+# disable Activex - the workaround suggested by Microsoft for the CVE-2021-40444
+# https://msrc.microsoft.com/update-guide/vulnerability/CVE-2021-40444
+#
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\0" -Force
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" -Force
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" -Force
