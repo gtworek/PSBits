4c6d511774f34d9dfcde706964b3417eccfeb571
Grzegorz Tworek <gtworek@users.noreply.github.com>
2022-01-22 11:00:34 +0100

    Create WordleHelper.ps1

---
+++ Misc/WordleHelper.ps1
@@ -0,0 +1,133 @@
+$wordlistURL = "https://github.com/dwyl/english-words/raw/master/words_alpha.txt" #other lists may require slightly different processing. 
+$wordlistRAW = Invoke-WebRequest -Uri $wordlistURL -UseBasicParsing
+$wordlist = $wordlistRAW.Content -split "\r\n" 
+Write-Host $wordlist.Count "words downloaded. Processing..."
+
+$newWordlist = @()
+foreach ($word in $wordlist)
+{
+    if (($word.Trim()).Length -eq 5)
+    {
+        $newWordlist += $word.ToUpper()
+    }
+}
+
+$wordlist = $newWordlist
+
+
+for ($j = 0; $j -lt 5; $j++)
+{
+
+    Write-Host $wordlist.Count "words matching."
+    if ("Y" -eq (Read-Host "Want to see them all? [Y/N]"))
+    {
+        if (Test-Path Variable:PSise)
+        {
+            $wordlist | Out-GridView
+            }
+        else
+        {
+            $wordlist | Format-Table
+        }
+    }
+
+    $guessOK = $false
+    while (!$guessOK)
+    {
+        $guess = Read-Host -Prompt "Type your guess"
+        if ($guess.Length -eq 5)
+        {
+            $guessOK = $true
+        }
+        else
+        {
+            Write-Host "Use 5-letters word!" -ForegroundColor Red
+        }
+    }
+
+    $guess = $guess.ToUpper()
+
+    if (!$wordlist.Contains($guess))
+    {
+        Write-Host "Your word list doesn't contain '$guess' ..." -ForegroundColor Yellow
+    }
+
+    $answerOK = $false
+    while (!$answerOK)
+    {
+        $answer = Read-Host -Prompt "Type wordle answer (B=black, Y=yellow, G=green)"
+        $answer = $answer.ToUpper()
+        if ($answer.Length -ne 5)
+        {
+            Write-Host "Use 5-letters answer!" -ForegroundColor Red
+            continue
+        }
+        $answerOK = $true
+        foreach ($letter in $answer.ToCharArray())
+        {
+            if (('B','Y','G') -notcontains $letter)
+            {
+                $answerOK = $false
+                Write-Host "Use only 'B', 'Y', and 'G' in Wordle answer!" -ForegroundColor Red
+            }
+        }
+    }
+
+
+
+    for ($i = 0; $i -lt 5; $i++)
+    {
+        $bc = switch (($answer.ToCharArray())[$i])
+        {
+            "B" {[System.ConsoleColor]::Black}
+            "Y" {[System.ConsoleColor]::Yellow}
+            "G" {[System.ConsoleColor]::Green}
+            default {[System.ConsoleColor]::Red}
+        }
+        Write-Host ($guess.ToCharArray())[$i] -NoNewline -ForegroundColor White -BackgroundColor $bc
+    }
+    Write-Host
+
+
+    for ($i = 0; $i -lt 5; $i++)
+    {
+        $newWordlist = @()
+        $guessLetter = ($guess.ToCharArray())[$i]
+        $answerLetter = ($answer.ToCharArray())[$i]
+        switch ($answerLetter)
+        {
+            "B" {
+                    foreach ($word in $wordlist)
+                    {
+                        if ($word.ToCharArray() -notcontains $guessLetter)
+                        {
+                            $newWordlist += $word
+                        }
+                    }
+                    break
+                }
+            "Y" {
+                    foreach ($word in $wordlist)
+                    {
+                        if ($word.ToCharArray() -contains $guessLetter)
+                        {
+                            $newWordlist += $word
+                        }
+                    }
+                    break
+                }
+            "G" {
+                    foreach ($word in $wordlist)
+                    {
+                        if ($word.ToCharArray()[$i] -eq $guessLetter)
+                        {
+                            $newWordlist += $word
+                        }
+                    }
+                    break
+                }
+        }
+        $wordlist = $newWordlist
+    }
+
+}
