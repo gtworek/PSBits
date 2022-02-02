423f0e86db848f6c32ccbe94d73fde94779ae6e4
Grzegorz Tworek <gtworek@users.noreply.github.com>
2022-02-02 07:49:28 +0100

    Create Get-AppLockerEvents.ps1
    Simple analysis of the "Microsoft-Windows-AppLocker/EXE and DLL" log

---
+++ Misc/Get-AppLockerEvents.ps1
@@ -0,0 +1,89 @@
+$evts = Get-WinEvent -LogName "Microsoft-Windows-AppLocker/EXE and DLL"
+
+$dll8002 = 0
+$dll8003 = 0
+$dll8004 = 0
+$exe8002 = 0
+$exe8003 = 0
+$exe8004 = 0
+$others = 0
+$blockedfiles = @()
+
+$sstr1 = " was allowed to run"
+$sstr2 = " was prevented from running"
+
+Write-Host $evts.Count "events to analyze"
+
+foreach ($evt in $evts)
+{
+    $filepath = ""
+    $id = $evt.Id
+    if (($id -eq 8002) -or ($id -eq 8003))
+    {
+        $filepath = $evt.Message.Substring(0,$evt.Message.IndexOf($sstr1))
+    }
+    if (($id -eq 8004))
+    {
+        $filepath = $evt.Message.Substring(0,$evt.Message.IndexOf($sstr2))
+    }
+    $filepath = $filepath.ToLower()
+    $isdll = (($filepath.EndsWith(".dll")) -or ($filepath.ToLower().EndsWith(".ocx")))
+    $isexe = $filepath.EndsWith(".exe")
+    if (($filepath -ne "") -and !$isdll -and !$isexe)
+    {
+        Write-Host "UNKNOWN EXT:" $filepath -ForegroundColor Red
+    }
+    if ($id -in (8002,8003,8004))
+    {
+        if (($id -eq 8002) -and ($isdll))
+        {
+            $dll8002 += 1
+        }
+        if (($id -eq 8003) -and ($isdll))
+        {
+            $dll8003 += 1
+            $blockedfiles += $filepath
+        }
+        if (($id -eq 8004) -and ($isdll))
+        {
+            $dll8004 += 1
+            $blockedfiles += $filepath
+        }
+        if (($id -eq 8002) -and ($isexe))
+        {
+            $exe8002 += 1
+        }
+        if (($id -eq 8003) -and ($isexe))
+        {
+            $exe8003 += 1
+            $blockedfiles += $filepath
+        }
+        if (($id -eq 8004) -and ($isexe))
+        {
+            $exe8004 += 1
+            $blockedfiles += $filepath
+        }
+    }
+    else
+    {
+        $others += 1
+    }
+}
+
+$blockedfiles = $blockedfiles | Sort-Object -Unique
+$oldesteventdatetime = (Get-WinEvent -LogName "Microsoft-Windows-AppLocker/EXE and DLL" -MaxEvents 1 -Oldest).TimeCreated
+
+Write-Host
+Write-Host `t`t"exe"`t"dll"
+Write-Host "8002"`t$exe8002`t$dll8002
+Write-Host "8003"`t$exe8003`t$dll8003
+Write-Host "8004"`t$exe8004`t$dll8004
+Write-Host "other events:" $others
+Write-Host
+Write-Host "Log file size:" (dir ([System.Environment]::ExpandEnvironmentVariables("%SystemRoot%\System32\Winevt\Logs\Microsoft-Windows-AppLocker%4EXE and DLL.evtx"))).Length
+Write-Host "Oldest event:" (Get-Date ($oldesteventdatetime) -Format "o")
+Write-Host "Events age:" (New-TimeSpan -Start $oldesteventdatetime -End (Get-Date))
+Write-Host 
+Write-Host "(Potentially) blocked files:" 
+$blockedfiles | ft
+
