d5bbe23a0b54c001bf805ff54850e6f89b8e69d9
Grzegorz Tworek <gtworek@users.noreply.github.com>
2022-04-12 21:17:48 +0200

    improvements, mainly about false positives.

--- Services/Get-SuspiciousServiceACLs.ps1
+++ Services/Get-SuspiciousServiceACLs.ps1
@@ -1,19 +1,15 @@
 # The script uses WMI to get ACLs for services and identify non-admin DC, WD or WO permissions.
-# Any of these permission allows instant elevation to localsystem for any user holding it.
+# Any of these permission allows instant elevation to localsystem for any user/group holding it.
 
-# Piece of theory - https://support.microsoft.com/en-us/help/914392/best-practices-and-guidance-for-writers-of-service-discretionary-acces
+# Piece of theory - https://support.microsoft.com/en-us/topic/3cf7240a-86ad-1fc3-bbb6-f468454981c4
 
 # TODO #1: Make it work remotely (easy as we use WMI here)
 # TODO #2: Declare suspicious privileges as an array variable instead of hardcoding strings in "if"
 # TODO #3: Error checking for resolving SIDs. Aliases resolving.
 # TODO #4: Make it return neat PS object instead of bunch of "write-host"
 
-# false positive may be reported for seclogon as it has cclcsWDtlocrrc which results from adjacent SW and DT - both quite innocent. Will be solved with TODO #2 from the list above
-
 ##############
-# no worries about TrustedInstaller as it extremely high privilege (IL)
-# no worries when services such DHCP have permissions for themself ("dhcp" identity). There is no risk related to such situation.
-# thanks @kaiserschloss for bringing this to my attention
+# thanks @kaiserschloss for comments
 ##############
 
 $DebugPreference = "Continue"
@@ -32,27 +28,42 @@ foreach ($srv in $services)
     {
         if ($ACE.Split(";")[0] -ne "A")
         {
-            continue #as we have non "allow something" ACE
+            continue #as we have non-"allow something" ACE
         }
     
-        # we should have set of permissions in the $ACE.Split(";")[2] 
-        # and the security principal (user/group/etc) in the $ACE.Split(";")[5]
-        
-        if ( ($ACE.Split(";")[2]).Contains("WD") -or ($ACE.Split(";")[2]).Contains("WO") -or ($ACE.Split(";")[2]).Contains("DC") )
+        # we should have set of permissions in the $ACE.Split(";")[2]. 
+        # Let's convert it to XX-YY-ZZ- format (in $dashRights) to avoid false positives.
+        # the security principal (user/group/etc) in the $ACE.Split(";")[5] ($PrincipalFromSDDL)
+        $dashRights = ""
+        for ($i = 0; $i -lt (($ACE.Split(";")[2]).Length); $i+=2)
         {
-            if ( (($ACE.Split(";")[5]) -eq "BA") -or (($ACE.Split(";")[5]) -eq "SY"))# we do not care about local administrators and localsystem as they should have such permissions
+            $dashRights += ($ACE.Split(";")[2]).Substring($i,2) + '-'
+        }
+        
+        $PrincipalFromSDDL = $ACE.Split(";")[5]
+
+        if ( $dashRights.Contains("WD") -or $dashRights.Contains("WO") -or $dashRights.Contains("DC") )
+        {
+            if ( ($PrincipalFromSDDL -eq "BA") -or ($PrincipalFromSDDL -eq "SY"))# we do not care about local administrators and localsystem as they should have such permissions
             {
                 continue
             }
-            $PrincipalName = ($ACE.Split(";")[5])
-            if ($PrincipalName.StartsWith("S-1-5-"))
+            $PrincipalName = $PrincipalFromSDDL
+            if ($PrincipalName.StartsWith("S-1-5-")) #sid and not alias
             {
-                $SID = New-Object System.Security.Principal.SecurityIdentifier(($ACE.Split(";")[5]))
+                $SID = New-Object System.Security.Principal.SecurityIdentifier($PrincipalFromSDDL)
                 $PrincipalName = $SID.Translate([System.Security.Principal.NTAccount]).Value
             }
-            Write-Host $srv.Name - $ACE.Split(";")[2] - $ACE.Split(";")[5] - $PrincipalName
+            if ($PrincipalName -eq "NT SERVICE\TrustedInstaller") #ignoring as it is safe
+            {
+                continue
+            }
+            if ($PrincipalName -eq ("NT SERVICE\"+$srv.Name)) #ignoring as it is safe
+            {
+                continue
+            }
+
+            Write-Host $srv.Name - $ACE.Split(";")[2] - $PrincipalFromSDDL - $PrincipalName
         } 
     }
 } 
-
-
