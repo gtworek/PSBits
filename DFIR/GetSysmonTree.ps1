8eae21d63e4775531b63083a588d4f60e763f9b3
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-07-05 12:08:33 +0200

    Update GetSysmonTree.ps1
    - Added documentation for Get-Help
    - optional parameter to specify the path to an existing log file. Default still is the currents system live log
    - Performance improvements by 
    	- querying event properties instead of parsing the events raw text
    	- reducing hash table lookups
    	- using enumerators
    - Provide more infos in the tooltips (pid, user, full command line)

--- DFIR/GetSysmonTree.ps1
+++ DFIR/GetSysmonTree.ps1
@@ -1,5 +1,29 @@
+<#
+.SYNOPSIS
+    Shows a process history tree with data extracted from a Sysmon/Operational log
+.EXAMPLE
+    PS> Get-SysmonTree.ps1
+
+    Shows the process tree of the current system, using the local event log
+    Depending on how the sysmon/operational log is configured, will usually require PS to run as admin
+.EXAMPLE
+    PS> Get-SysmonTree.ps1 -LogPath "~\Desktop\Microsoft-Windows-Sysmon%4Operational.evtx.evtx"
+
+    Shows the process tree using a previously saved Sysmon log
+#>
+
+[CmdletBinding(DefaultParameterSetName='Live')]
+
+Param (
+    # The Path to a Sysmon/Operational log file
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ParameterSetName='FromLog')]
+    [ValidateNotNullOrEmpty()]
+    [string] $LogPath
+)
+
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void][System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+
 # create Form for displaying the folder tree
 $Form = New-Object System.Windows.Forms.Form
 $Form.Text = "Sysmon Process Tree"
@@ -7,7 +31,7 @@ $Form.Size = New-Object System.Drawing.Size(640, 480)
 # create Treeview-Object
 $TreeView = New-Object System.Windows.Forms.TreeView
 $TreeView.Dock = [System.Windows.Forms.DockStyle]::Fill
-$TreeView.Sorted = $true ###########
+$TreeView.Sorted = $true
 $TreeView.ShowNodeToolTips = $true
 $Form.Controls.Add($TreeView)
 
@@ -18,95 +42,88 @@ $TreeView.Nodes.Clear()
 $nodesHT = @{}
 
 #tree node for unknowns
-$node = New-Object System.Windows.Forms.TreeNode
-$nodesrow = New-Object psobject
-$nodesrow | Add-Member -Name "TreeNode" -Value $node -MemberType NoteProperty
-$nodesHT[$unknownGUID] = $nodesrow
-$nodesHT[$unknownGUID].TreeNode.Text = "UNKNOWN"
-$nodesHT[$unknownGUID].TreeNode.Name = $unknownGUID
-$nodesHT[$unknownGUID].TreeNode.ToolTipText = 'Sysmon did not report any parent'
-[void]$TreeView.Nodes.Add($nodesHT[$unknownGUID].TreeNode)
+$newNode = [PSCustomObject] @{ TreeNode = New-Object System.Windows.Forms.TreeNode }
+$newNode.TreeNode.Text = "UNKNOWN"
+$newNode.TreeNode.Name = $unknownGUID
+$newNode.TreeNode.ToolTipText = 'Sysmon did not report any parent'
+$nodesHT[$unknownGUID] = $newNode
+[void] $TreeView.Nodes.Add($newNode.TreeNode)
 
 #tree node for orphans
-$node = New-Object System.Windows.Forms.TreeNode
-$nodesrow = New-Object psobject
-$nodesrow | Add-Member -Name "TreeNode" -Value $node -MemberType NoteProperty
-$nodesHT[$orphanGUID] = $nodesrow
-$nodesHT[$orphanGUID].TreeNode.Text = "ORPHAN"
-$nodesHT[$orphanGUID].TreeNode.Name = $orphanGUID
-$nodesHT[$orphanGUID].TreeNode.ToolTipText = 'Parent not found in the event log'
-[void]$TreeView.Nodes.Add($nodesHT[$orphanGUID].TreeNode)
-
-#headers of data to extract from event text
-$cmdLineHeader = "Image: "
-$parentCmdLineHeader = "ParentImage: "
-$pGUIDHeader = "ProcessGuid: "
-$ppGUIDHeader = "ParentProcessGuid: "
-$UTCTimeHeader = "UtcTime: "
+$nodeHT = [PSCustomObject] @{ TreeNode = New-Object System.Windows.Forms.TreeNode }
+$nodeHT.TreeNode.Text = "ORPHAN"
+$nodeHT.TreeNode.Name = $orphanGUID
+$nodeHT.TreeNode.ToolTipText = 'Parent not found in the event log'
+$nodesHT[$orphanGUID] = $nodeHT
+[void] $TreeView.Nodes.Add($nodeHT.TreeNode)
 
 #getting all events. Feel free to change xpath i.e. limiting time range etc.
-Write-Host "Getting events, it may take a while..." -NoNewline
-$evts = (Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" -FilterXPath "*[System[EventID=1]]")
-Write-Host "Done."
+Write-Host "Querying events, may take a while..."
+if($PSCmdlet.ParameterSetName -eq "FromLog") {
+    $evts = (Get-WinEvent -Path $LogPath -FilterXPath "*[System[EventID=1]]")
+} else {
+    try {
+        $evts = (Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" -FilterXPath "*[System[EventID=1]]")
+    } catch {
+        throw "Exception Querying the Sysmon/Operational log. Missing Administrator rights?"
+    }
+}
+Write-Host "Done. Generating output..."
 
 $evtHT = @{}
 
 #go through log and convert events into hashtable
-foreach ($evt in $evts)
-{
-    $msg = $evt.Message
-    $cmdline = $msg.Substring($msg.IndexOf($CmdLineHeader) + $CmdLineHeader.Length)
-    $cmdline = $cmdline.Substring(0,$cmdline.IndexOf("`r`n"))
-    $parentCmdline = $msg.Substring($msg.IndexOf($parentCmdLineHeader) + $parentCmdLineHeader.Length)
-    $parentCmdLine = $parentCmdLine.Substring(0,$parentCmdLine.IndexOf("`r`n"))
-    $pGUID = $msg.Substring($msg.IndexOf($pGUIDHeader) + $pGUIDHeader.Length)
-    $pGUID = $pGUID.Substring(0,$pGUID.IndexOf("`r`n"))
-    $ppGUID = $msg.Substring($msg.IndexOf($ppGUIDHeader) + $ppGUIDHeader.Length)
-    $ppGUID = $ppGUID.Substring(0,$ppGUID.IndexOf("`r`n"))
-    $UTCTime = $msg.Substring($msg.IndexOf($UTCTimeHeader) + $UTCTimeHeader.Length)
-    $UTCTime = $UTCTime.Substring(0,$UTCTime.IndexOf("`r`n"))
-
-    $evtrow = New-Object psobject
-    $evtrow | Add-Member -Name "cmdline" -Value $cmdline -MemberType NoteProperty
-    $evtrow | Add-Member -Name "parentCmdline" -Value $parentCmdline -MemberType NoteProperty
-    $evtrow | Add-Member -Name "pGUID" -Value $pGUID -MemberType NoteProperty
-    $evtrow | Add-Member -Name "ppGUID" -Value $ppGUID -MemberType NoteProperty
-    $evtrow | Add-Member -Name "UTCTime" -Value ("Start UTC: "+$UTCTime) -MemberType NoteProperty
-    $evtHT[$pGUID] = $evtrow
+foreach ($evt in $evts) {
+    $pGUID = $evt.Properties[2].Value.Guid
+    $evtHT[$pGUID] = [PSCustomObject] @{
+    	image = $evt.Properties[4].Value
+    	cmdline = $evt.Properties[10].Value
+    	parentCmdline = $evt.Properties[21].Value
+    	pGUID = $pGUID
+    	ppGUID = $evt.Properties[18].Value.Guid
+    	UTCTime = $evt.Properties[1].Value
+    	pid = $evt.Properties[3].Value
+        user = $evt.Properties[12].Value
+    }
 }
 
 #create nodes, but not "attach" them. It will make parent search possible.
-foreach ($evtrow in $evtHT.Keys)
-{
+foreach ($entry in $evtHT.GetEnumerator()) {
+    $evtrow = $entry.Key
+    $evtHTEntry = $entry.Value
+
     $node = New-Object System.Windows.Forms.TreeNode
-    $nodesrow = New-Object psobject
-    $nodesrow | Add-Member -Name "TreeNode" -Value $node -MemberType NoteProperty
-    $nodesHT[$evtrow] = $nodesrow
-    $nodesHT[$evtrow].TreeNode.Text = $evtHT[$evtrow].cmdline
-    $nodesHT[$evtrow].TreeNode.Name = $evtrow
-    $nodesHT[$evtrow].TreeNode.ToolTipText = $evtHT[$evtrow].UTCTime
+    $newNode = [PSCustomObject] @{ TreeNode = $node }
+    $newNode.TreeNode.Text = $evtHTEntry.image
+    $newNode.TreeNode.Name = $evtrow
+    $newNode.TreeNode.ToolTipText = @"
+Start UTC: $($evtHTEntry.UTCTime)
+PID: $($evtHTEntry.pid)
+User: $($evtHTEntry.user)
+Command Line: $($evtHTEntry.cmdline)
+"@
+
+    $nodesHT[$evtrow] = $newNode
 }
 
 #go through events again and "attach" each entry to its parent
-foreach ($evtrow in $evtHT.Keys)
-{
-    if (($evtHT[$evtrow].cmdline -eq 'C:\Windows\System32\smss.exe') -and ($evtHT[$evtrow].parentCmdline -eq 'System'))
-    {
-        $nodesHT[$evtrow].TreeNode.Text = "OS Start: "+$evtHT[$evtrow].UTCTime+" UTC | " +$nodesHT[$evtrow].TreeNode.Text
-        [void]$TreeView.Nodes.Add($nodesHT[$evtrow].TreeNode)
+foreach ($entry in $evtHT.GetEnumerator()) {
+    $evtrow = $entry.Key
+    $evtHTEntry = $entry.Value
+    $entNodesHTEntry = $nodesHT[$evtrow]
+
+    if (($evtHTEntry.cmdline -eq 'C:\Windows\System32\smss.exe') -and ($evtHTEntry.parentCmdline -eq 'System')) {
+        $entNodesHTEntry.TreeNode.Text = "OS Start: " + $evtHTEntry.UTCTime + " UTC | " + $entNodesHTEntry.TreeNode.Text
+        [void] $TreeView.Nodes.Add($nodesHT[$evtrow].TreeNode)
         continue
     }
 
-    if ($evtHT[$evtHT[$evtrow].ppGUID] -eq $null)
-    {
-        if ($evtHT[$evtrow].ppGUID -ne $unknownGUID)
-        {
-            $evtHT[$evtrow].ppGUID = $orphanGUID
-        }
+    if (($evtHT[$evtHTEntry.ppGUID] -eq $null) -and ($evtHTEntry.ppGUID -ne $unknownGUID)) {
+        $evtHTEntry.ppGUID = $orphanGUID
     }
-    [void]$nodesHT[$evtHT[$evtrow].ppGUID].TreeNode.Nodes.Add($nodesHT[$evtrow].TreeNode)
+    
+    [void] $nodesHT[$evtHTEntry.ppGUID].TreeNode.Nodes.Add($entNodesHTEntry.TreeNode)
 }
-
 
 $Form.Add_Shown( { $Form.Activate() })
 [void] $Form.ShowDialog()
