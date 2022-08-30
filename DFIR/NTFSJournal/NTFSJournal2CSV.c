e477adfb15d896fbcd27b0e1a7a5ffd006c79713
Grzegorz Tworek <gtworek@users.noreply.github.com>
2022-08-30 08:46:11 +0200

    Code cleanup

--- DFIR/NTFSJournal/NTFSJournal2CSV.c
+++ DFIR/NTFSJournal/NTFSJournal2CSV.c
@@ -2,6 +2,7 @@
 #include <tchar.h>
 #include <Windows.h>
 
+#define DOT_INTERVAL (100 * 1000)
 #define READ_JOURNAL_BUFFER_SIZE (1024 * 1024)
 #define ISO_DATETIME_LEN 26
 #define ROW_LEN 4096
@@ -28,24 +29,9 @@ PTCHAR pszVolumeName;
 PTCHAR pszCsvFileName;
 
 
-
-__inline PUSN_RECORD_V3 NextUsnRecord(PUSN_RECORD_V3 currentRecord)
-{
-	ULONGLONG newRecord;
-	(PUSN_RECORD_V3)newRecord = currentRecord;
-	newRecord += currentRecord->RecordLength;
-	if (newRecord & 8 - 1) // align if needed
-	{
-		newRecord &= -8;
-		newRecord += 8;
-	}
-	return((PUSN_RECORD_V3)newRecord);
-}
-
 PTCHAR TimeStampToIso8601(
 	LARGE_INTEGER* timeStamp,
-	PTCHAR pszBuffer,
-	size_t cchBufferSize
+	PTCHAR pszBuffer
 )
 {
 	SYSTEMTIME systemTime;
@@ -55,7 +41,8 @@ PTCHAR TimeStampToIso8601(
 	}
 	if (FileTimeToSystemTime((PFILETIME)timeStamp, &systemTime))
 	{
-		StringCchPrintf(pszBuffer,
+		StringCchPrintf(
+			pszBuffer,
 			ISO_DATETIME_LEN,
 			TEXT("%04i-%02i-%02iT%02i:%02i:%02i.%03iZ"),
 			systemTime.wYear,
@@ -70,8 +57,9 @@ PTCHAR TimeStampToIso8601(
 	return pszBuffer;
 }
 
+
 PTCHAR FileId128ToHex(
-	PUCHAR fileId128,
+	PVOID fileId128,
 	PTCHAR pszBuffer,
 	size_t cchBufferSize
 )
@@ -95,20 +83,19 @@ PTCHAR FileId128ToHex(
 	return pszBuffer;
 }
 
+
 int _tmain(int argc, PTCHAR argv[])
 {
-	if (argc == 3)
-	{
-		pszVolumeName = argv[1];
-		pszCsvFileName = argv[2];
-	}
-	else
+	if (argc < 3)
 	{
 		_tprintf(TEXT("Usage: NTFSJournal2CSV.exe \\\\.\\C: c:\\journal.csv\r\n"));
 		return ERROR_INVALID_PARAMETER;
 	}
 
-	_tprintf(TEXT("Trying to open %s ..."), pszVolumeName);
+	pszVolumeName = argv[1];
+	pszCsvFileName = argv[2];
+
+	_tprintf(TEXT("Trying to create %s ..."), pszVolumeName);
 	hUsnFileHandle = CreateFile(
 		pszVolumeName,
 		FILE_WRITE_ATTRIBUTES,
@@ -121,12 +108,9 @@ int _tmain(int argc, PTCHAR argv[])
 	if (INVALID_HANDLE_VALUE == hUsnFileHandle)
 	{
 		_tprintf(TEXT("\r\nERROR: CreateFile() returned %u\r\n"), GetLastError());
-		return GetLastError();
+		return (int)GetLastError();
 	}
-	else
-	{
-		_tprintf(TEXT(" Success.\r\n"));
-	}
+	_tprintf(TEXT(" Success.\r\n"));
 	_tprintf(TEXT("Trying to open %s ..."), pszCsvFileName);
 	hCsvFileHandle = CreateFile(
 		pszCsvFileName,
@@ -140,13 +124,14 @@ int _tmain(int argc, PTCHAR argv[])
 	if (INVALID_HANDLE_VALUE == hCsvFileHandle)
 	{
 		_tprintf(TEXT("\r\nERROR: CreateFile() returned %u\r\n"), GetLastError());
-		return GetLastError();
-	}
-	else
-	{
-		_tprintf(TEXT(" Success.\r\n"));
+		if (ERROR_FILE_EXISTS == GetLastError()) //most common case
+		{
+			_tprintf(TEXT("ERROR: The file %s already exists.\r\n"), pszCsvFileName);
+		}
+		return (int)GetLastError();
 	}
 
+	_tprintf(TEXT(" Success.\r\n"));
 	_tprintf(TEXT("Sending FSCTL_QUERY_USN_JOURNAL\r\n"));
 	bStatus = DeviceIoControl(
 		hUsnFileHandle,
@@ -165,7 +150,7 @@ int _tmain(int argc, PTCHAR argv[])
 			TEXT("ERROR: DeviceIoControl(FSCTL_QUERY_USN_JOURNAL...) returned %u\r\n"),
 			GetLastError()
 		);
-		return GetLastError();
+		return (int)GetLastError();
 	}
 
 	_tprintf(TEXT("Journal found on %s\r\n"), pszVolumeName);
@@ -180,13 +165,13 @@ int _tmain(int argc, PTCHAR argv[])
 	_tprintf(TEXT(" MaxSupportedMajorVersion : %u\r\n"), UsnJournalData.MaxSupportedMajorVersion);
 
 	ReadUsnJournalDataV1.UsnJournalID = UsnJournalData.UsnJournalID;
-	ReadUsnJournalDataV1.ReasonMask = ~0;
+	ReadUsnJournalDataV1.ReasonMask = MAXDWORD;
 	ReadUsnJournalDataV1.ReturnOnlyOnClose = FALSE;
 	ReadUsnJournalDataV1.Timeout = 0;
 	ReadUsnJournalDataV1.MinMajorVersion = 2;
 	ReadUsnJournalDataV1.MaxMajorVersion = 4;
 
-	lpBuffer = (PVOID)malloc(READ_JOURNAL_BUFFER_SIZE);
+	lpBuffer = malloc(READ_JOURNAL_BUFFER_SIZE);
 
 	if (!lpBuffer)
 	{
@@ -216,11 +201,8 @@ int _tmain(int argc, PTCHAR argv[])
 			{
 				break;
 			}
-			else
-			{
-				_tprintf(TEXT("ERROR: DeviceIoControl(FSCTL_READ_USN_JOURNAL...) returned %u\r\n"), GetLastError());
-				return GetLastError();
-			}
+			_tprintf(TEXT("ERROR: DeviceIoControl(FSCTL_READ_USN_JOURNAL...) returned %u\r\n"), GetLastError());
+			return (int)GetLastError();
 		}
 
 		if (dwBytesReturned < sizeof(ULONGLONG) + sizeof(USN_RECORD))
@@ -228,71 +210,75 @@ int _tmain(int argc, PTCHAR argv[])
 			break;
 		}
 
-		UsnRecordV3 = (PUSN_RECORD_V3)((PBYTE)lpBuffer + sizeof(ULONGLONG));
-		while (((PBYTE)UsnRecordV3 < (PBYTE)lpBuffer + dwBytesReturned) && ((PBYTE)UsnRecordV3 + UsnRecordV3->RecordLength <= (PBYTE)lpBuffer + dwBytesReturned))
+		UsnRecordV3 = Add2Ptr(lpBuffer, sizeof(ULONGLONG));
+		while (((PVOID)UsnRecordV3 < Add2Ptr(lpBuffer, dwBytesReturned)) &&
+			(Add2Ptr(UsnRecordV3, UsnRecordV3->RecordLength) <= Add2Ptr(lpBuffer, dwBytesReturned)))
 		{
+			WCHAR wszFileName[MAX_PATH] = {0};
 			lCounter++;
-			switch (UsnRecordV3->MajorVersion) //ver 2, 3, and 4 have the same structure fields at the beginning. Even if it is not ver 3, the code is ok.
+			switch (UsnRecordV3->MajorVersion)
+			//ver 2, 3, and 4 have the same structure fields at the beginning. Even if it is not ver 3, the code is ok.
 			{
-				case 3: //only ver 3 is supported, as it is the only one happening in disks I had a chance to analyze.
-					hResult= StringCchPrintf(
-						strBuf,
-						ROW_LEN,
-						TEXT("%lld\t0x%08x\t0x%08x\t0x%08x\t0x%08x\t%s\t%s\t%s\t%.*s\r\n"),
-						UsnRecordV3->Usn,
-						UsnRecordV3->Reason,
-						UsnRecordV3->SourceInfo,
-						UsnRecordV3->SecurityId,
-						UsnRecordV3->FileAttributes,
-						TimeStampToIso8601(&UsnRecordV3->TimeStamp, strIsoDateTime, ISO_DATETIME_LEN),
-						FileId128ToHex(UsnRecordV3->FileReferenceNumber.Identifier, strFileId, _countof(strFileId)),
-						FileId128ToHex(UsnRecordV3->ParentFileReferenceNumber.Identifier, strParentFileId, _countof(strParentFileId)),
-						UsnRecordV3->FileNameLength / sizeof(WCHAR), //wchar as it is about wide string defined in struct; value used to cut name in the next param
-						(PWSTR)Add2Ptr(UsnRecordV3, UsnRecordV3->FileNameOffset) //always PWSTR as it is how it goes in struct
-					);
+			case 3: //only ver 3 is supported, as it is the only one happening in disks I had a chance to analyze.
+				memcpy(wszFileName, Add2Ptr(UsnRecordV3, UsnRecordV3->FileNameOffset), UsnRecordV3->FileNameLength);
+				hResult = StringCchPrintf(
+					strBuf,
+					ROW_LEN,
+					TEXT("%lld;%08x;%08x;%08x;%08x;%s;%s;%s;\"%ls\"\r\n"),
+					UsnRecordV3->Usn,
+					UsnRecordV3->Reason,
+					UsnRecordV3->SourceInfo,
+					UsnRecordV3->SecurityId,
+					UsnRecordV3->FileAttributes,
+					TimeStampToIso8601(&UsnRecordV3->TimeStamp, strIsoDateTime),
+					FileId128ToHex(UsnRecordV3->FileReferenceNumber.Identifier, strFileId, _countof(strFileId)),
+					FileId128ToHex(
+						UsnRecordV3->ParentFileReferenceNumber.Identifier,
+						strParentFileId,
+						_countof(strParentFileId)),
+					wszFileName
+				);
 				if (S_OK != hResult)
 				{
 					_tprintf(TEXT("ERROR: StringCchPrintf returned %u\r\n"), hResult);
 					return ERROR_INSUFFICIENT_BUFFER;
 				}
-#pragma warning( disable : 4267 ) //size_t to dword. Should be safe because size of TCHAR should not overflow DWORD
-					bStatus = WriteFile(
-						hCsvFileHandle,
-						strBuf,
-						_tcslen(strBuf) * sizeof(TCHAR),
-						&dwWritten,
-						NULL
-					);
-#pragma warning( default : 4267 ) //back to normal
-					if (!bStatus)
-					{
-						_tprintf(
-							TEXT("ERROR: WriteFile() returned %u\r\n"),
-							GetLastError()
-						);
-						return GetLastError();
-					}
-					break; //ver 3
 
-				default:
-					_tprintf(TEXT("Unknown record version. Ver. %i.%i. Length: %i\r\n"),
-						UsnRecordV3->MajorVersion,
-						UsnRecordV3->MinorVersion,
-						UsnRecordV3->RecordLength);
-					return ERROR_UNSUPPORTED_TYPE;
-					break;
+				bStatus = WriteFile(
+					hCsvFileHandle,
+					strBuf,
+					(DWORD)(_tcslen(strBuf) * sizeof(TCHAR)),
+					&dwWritten,
+					NULL
+				);
+				if (!bStatus)
+				{
+					_tprintf(
+						TEXT("ERROR: WriteFile() returned %u\r\n"),
+						GetLastError()
+					);
+					return (int)GetLastError();
+				}
+				break; //ver 3
+
+			default:
+				_tprintf(
+					TEXT("Unknown record version. Ver. %i.%i. Length: %i\r\n"),
+					UsnRecordV3->MajorVersion,
+					UsnRecordV3->MinorVersion,
+					UsnRecordV3->RecordLength);
+				return ERROR_UNSUPPORTED_TYPE;
 			}
-			UsnRecordV3 = NextUsnRecord(UsnRecordV3);
-			if (0 == (lCounter % 100000))
+			UsnRecordV3 = Add2Ptr(UsnRecordV3, UsnRecordV3->RecordLength); //assuming proper alignment
+			if (0 == (lCounter % DOT_INTERVAL))
 			{
 				_tprintf(TEXT("."));
 			}
 		} //inner while
 		ReadUsnJournalDataV1.StartUsn = *(USN*)lpBuffer;
 	} //while true
-	_tprintf(TEXT("\r\nDone. %ld entries processed.\r\n"), lCounter);
+	_tprintf(TEXT("\r\n%ld entries processed.\r\nDone.\r\n"), lCounter);
 	CloseHandle(hUsnFileHandle);
 	CloseHandle(hCsvFileHandle);
 	return 0;
 }
-
