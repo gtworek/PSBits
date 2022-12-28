02147d9e27ecef5227d4aeafe78760b194bca0a4
Grzegorz Tworek <gtworek@users.noreply.github.com>
2022-12-28 18:46:48 +0100

    Create Sysmon2CSV.ps1

---
+++ Sysmon/Sysmon2CSV.ps1
@@ -0,0 +1,28 @@
+$maxEvents = [long]::MaxValue #limit for the last X events from the log if you like
+$destinationFolder = [Environment]::GetFolderPath("Desktop")
+$destinationFile = (Get-Date -Format yyyyMMddTHHmmss)
+
+Write-Host 'Getting events, may take a while...'
+$evts = (Get-WinEvent -LogName "Microsoft-Windows-Sysmon/Operational" -FilterXPath "*[System[EventID=1]]" -MaxEvents $maxEvents).Message
+
+$i=0
+$evtObjects = @()
+foreach ($evt in $evts)
+{
+    Write-Progress -PercentComplete (($i++)*100/$evts.Count) -Activity "Analyzing..."
+    $row = New-Object psobject
+    foreach ($line in $evt.Split("`r`n"))
+    {
+        $colonPos = $line.IndexOf(': ')
+        if ($colonPos -eq -1)
+        {
+            continue
+        }
+        $memberName = $line.Substring(0,$colonPos)
+        $memberValue = $line.Substring($colonPos+2, $line.Length-$memberName.Length-2)
+        $row | Add-Member -Name ($memberName) -MemberType NoteProperty -Value ($memberValue)
+    }
+    $evtObjects += $row
+}
+
+$evtObjects | Export-Csv -LiteralPath ($destinationFolder + $destinationFile + '.csv')
