0f9119680f0adbc3cc12c62832f361ab6509ddea
Grzegorz Tworek <gtworek@users.noreply.github.com>
2023-05-15 16:54:02 +0200

    Improved

--- VirtualAccounts/RunAsVA.c
+++ VirtualAccounts/RunAsVA.c
@@ -1,10 +1,15 @@
+#ifndef UNICODE
+#error Unicode environment required due to LogonUserExExW().
+#endif
+
 #include <Windows.h>
-#include <tchar.h>
-#include <strsafe.h>
+#include <wchar.h>
 
-#define BASE_RID 97
+#pragma comment(lib, "ntdll.lib")
 
-#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)   
+#define BASE_RID 97 //feel free to change
+
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
 #define STATUS_PRIVILEGE_NOT_HELD ((NTSTATUS)0xc0000061L)
 
 typedef struct _UNICODE_STRING
@@ -57,7 +62,6 @@ typedef union _LSA_SID_NAME_MAPPING_OPERATION_INPUT
 	LSA_SID_NAME_MAPPING_OPERATION_ADD_INPUT AddInput;
 	LSA_SID_NAME_MAPPING_OPERATION_REMOVE_INPUT RemoveInput;
 	LSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_INPUT AddMultipleInput;
-
 } LSA_SID_NAME_MAPPING_OPERATION_INPUT, *PLSA_SID_NAME_MAPPING_OPERATION_INPUT;
 
 typedef struct _LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT
@@ -65,9 +69,12 @@ typedef struct _LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT
 	LSA_SID_NAME_MAPPING_OPERATION_ERROR ErrorCode;
 } LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT;
 
-typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT LSA_SID_NAME_MAPPING_OPERATION_ADD_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_ADD_OUTPUT;
-typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT LSA_SID_NAME_MAPPING_OPERATION_REMOVE_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_REMOVE_OUTPUT;
-typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT LSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_OUTPUT;
+typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT
+	LSA_SID_NAME_MAPPING_OPERATION_ADD_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_ADD_OUTPUT;
+typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT
+	LSA_SID_NAME_MAPPING_OPERATION_REMOVE_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_REMOVE_OUTPUT;
+typedef LSA_SID_NAME_MAPPING_OPERATION_GENERIC_OUTPUT
+	LSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_OUTPUT;
 
 typedef union _LSA_SID_NAME_MAPPING_OPERATION_OUTPUT
 {
@@ -76,28 +83,30 @@ typedef union _LSA_SID_NAME_MAPPING_OPERATION_OUTPUT
 	LSA_SID_NAME_MAPPING_OPERATION_ADD_MULTIPLE_OUTPUT AddMultipleOutput;
 } LSA_SID_NAME_MAPPING_OPERATION_OUTPUT, *PLSA_SID_NAME_MAPPING_OPERATION_OUTPUT;
 
-
 NTSTATUS
 NTAPI
 LsaManageSidNameMapping(
-	_In_    LSA_SID_NAME_MAPPING_OPERATION_TYPE     OperationType,
-	_In_    PLSA_SID_NAME_MAPPING_OPERATION_INPUT   OperationInput,
-	_Out_   PLSA_SID_NAME_MAPPING_OPERATION_OUTPUT* OperationOutput
+	_In_ LSA_SID_NAME_MAPPING_OPERATION_TYPE OperationType,
+	_In_ PLSA_SID_NAME_MAPPING_OPERATION_INPUT OperationInput,
+	_Out_ PLSA_SID_NAME_MAPPING_OPERATION_OUTPUT* OperationOutput
 );
 
-
-NTSTATUS
-LsaFreeMemory(
+NTSTATUS LsaFreeMemory(
 	PVOID Buffer
 );
 
+VOID
+NTAPI
+RtlInitUnicodeString(
+	PUNICODE_STRING DestinationString,
+	PCWSTR SourceString
+);
 
-NTSTATUS
-AddSidMapping(
+NTSTATUS AddSidMapping(
 	PUNICODE_STRING pDomainName,
 	PUNICODE_STRING pAccountName,
-	PSID pSid,
-	PLSA_SID_NAME_MAPPING_OPERATION_ERROR pResult)
+	PSID pSid
+)
 {
 	NTSTATUS status;
 	LSA_SID_NAME_MAPPING_OPERATION_INPUT opInput = {0};
@@ -113,66 +122,71 @@ AddSidMapping(
 	opInput.AddInput.Sid = pSid;
 
 	// Do the real job
-	status = LsaManageSidNameMapping(
-		LsaSidNameMappingOperation_Add,
-		&opInput,
-		&pOpOutput
-	);
+	status = LsaManageSidNameMapping(LsaSidNameMappingOperation_Add, &opInput, &pOpOutput);
 
 	// Treat repeated mappings as success.
-	if (LsaSidNameMappingOperation_NameCollision == pOpOutput->AddOutput.ErrorCode || LsaSidNameMappingOperation_SidCollision == pOpOutput->AddOutput.ErrorCode)
+	if (LsaSidNameMappingOperation_NameCollision == pOpOutput->AddOutput.ErrorCode ||
+		LsaSidNameMappingOperation_SidCollision == pOpOutput->AddOutput.ErrorCode)
 	{
+		wprintf(L"LsaManageSidNameMapping(Add) error code: %ld\r\n", pOpOutput->AddOutput.ErrorCode);
 		status = STATUS_SUCCESS;
 	}
 
-	*pResult = pOpOutput->AddOutput.ErrorCode;
 	LsaFreeMemory(pOpOutput);
 	return status;
 }
 
-
-// no length checking. No real reason to expect input longer than USHORT/2.
-VOID
-InitUnicodeString(
-	PUNICODE_STRING DestinationString,
-	PCWSTR SourceString
+NTSTATUS RemoveSidMapping(
+	PUNICODE_STRING pDomainName,
+	PUNICODE_STRING pAccountName
 )
 {
-	USHORT length;
-	DestinationString->Buffer = (PWSTR)SourceString;
-	length = (USHORT)wcslen(SourceString) * sizeof(WCHAR);
-	DestinationString->Length = (USHORT)length;
-	DestinationString->MaximumLength = (USHORT)(length + sizeof(UNICODE_NULL));
+	NTSTATUS status;
+	LSA_SID_NAME_MAPPING_OPERATION_INPUT opInput = {0};
+	LSA_SID_NAME_MAPPING_OPERATION_INPUT opInput2 = {0};
+	PLSA_SID_NAME_MAPPING_OPERATION_OUTPUT pOpOutput = NULL;
+
+	opInput.RemoveInput.DomainName = *pDomainName;
+	opInput.RemoveInput.AccountName = *pAccountName;
+	status = LsaManageSidNameMapping(LsaSidNameMappingOperation_Remove, &opInput, &pOpOutput);
+	LsaFreeMemory(pOpOutput);
+	wprintf(L"\r\nLsaManageSidNameMapping(Remove) #1 returned %ld\r\n", status);
+
+	opInput2.RemoveInput.DomainName = *pDomainName;
+	status = LsaManageSidNameMapping(LsaSidNameMappingOperation_Remove, &opInput2, &pOpOutput);
+	LsaFreeMemory(pOpOutput);
+	wprintf(L"LsaManageSidNameMapping(Remove) #2 returned %ld\r\n", status);
+	return status; //return status #2
 }
 
 
 // the stuff for dynamically load LogonUserExExW
-typedef BOOL(*LOGON_USER_EXEXW)(
-	_In_      LPTSTR        lpszUsername,
-	_In_opt_  LPTSTR        lpszDomain,
-	_In_opt_  LPTSTR        lpszPassword,
-	_In_      DWORD         dwLogonType,
-	_In_      DWORD         dwLogonProvider,
-	_In_opt_  PTOKEN_GROUPS pTokenGroups,
-	_Out_opt_ PHANDLE       phToken,
+typedef BOOL (*LOGON_USER_EXEXW)(
+	_In_ LPTSTR lpszUsername,
+	_In_opt_ LPTSTR lpszDomain,
+	_In_opt_ LPTSTR lpszPassword,
+	_In_ DWORD dwLogonType,
+	_In_ DWORD dwLogonProvider,
+	_In_opt_ PTOKEN_GROUPS pTokenGroups,
+	_Out_opt_ PHANDLE phToken,
 	_Out_opt_ PSID* ppLogonSid,
 	_Out_opt_ PVOID* ppProfileBuffer,
-	_Out_opt_ LPDWORD       pdwProfileLength,
+	_Out_opt_ LPDWORD pdwProfileLength,
 	_Out_opt_ PQUOTA_LIMITS pQuotaLimits
-	);
+);
 
 // dynamically loaded from Advapi32.dll
 BOOL WINAPI LogonUserExExW(
-	LPTSTR        lpszUsername,
-	LPTSTR        lpszDomain,
-	LPTSTR        lpszPassword,
-	DWORD         dwLogonType,
-	DWORD         dwLogonProvider,
+	LPTSTR lpszUsername,
+	LPTSTR lpszDomain,
+	LPTSTR lpszPassword,
+	DWORD dwLogonType,
+	DWORD dwLogonProvider,
 	PTOKEN_GROUPS pTokenGroups,
-	PHANDLE       phToken,
+	PHANDLE phToken,
 	PSID* ppLogonSid,
 	PVOID* ppProfileBuffer,
-	LPDWORD       pdwProfileLength,
+	LPDWORD pdwProfileLength,
 	PQUOTA_LIMITS pQuotaLimits
 )
 {
@@ -181,25 +195,25 @@ BOOL WINAPI LogonUserExExW(
 
 	if (NULL == hAdvapi32Module)
 	{
-		hAdvapi32Module = LoadLibraryEx(L"Advapi32.dll", NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
+		hAdvapi32Module = LoadLibraryExW(L"Advapi32.dll", NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
 		if (NULL == hAdvapi32Module)
 		{
-			_tprintf(TEXT("LoadLibraryEx() returned %ld\r\n"), GetLastError());
-			exit(GetLastError());
+			wprintf(L"LoadLibraryExW() returned %ld\r\n", GetLastError());
+			_exit((int)GetLastError());
 		}
-		pfnLogonUserExExW = (LOGON_USER_EXEXW)GetProcAddress(hAdvapi32Module, "LogonUserExExW");
+		pfnLogonUserExExW = (LOGON_USER_EXEXW)(LPVOID)GetProcAddress(hAdvapi32Module, "LogonUserExExW");
 		if (NULL == pfnLogonUserExExW)
 		{
-			_tprintf(TEXT("GetProcAddress() returned %ld\r\n"), GetLastError());
-			exit(GetLastError());
+			wprintf(L"GetProcAddress() returned %ld\r\n", GetLastError());
+			_exit((int)GetLastError());
 		}
 	}
 	if (NULL == pfnLogonUserExExW)
 	{
-		_tprintf(TEXT("The specified procedure could not be found.\r\n"));
-		exit(ERROR_PROC_NOT_FOUND);
+		wprintf(L"The specified procedure could not be found.\r\n");
+		_exit(ERROR_PROC_NOT_FOUND);
 	}
-	
+
 	return pfnLogonUserExExW(
 		lpszUsername,
 		lpszDomain,
@@ -211,13 +225,14 @@ BOOL WINAPI LogonUserExExW(
 		ppLogonSid,
 		ppProfileBuffer,
 		pdwProfileLength,
-		pQuotaLimits
-	);
+		pQuotaLimits);
 }
 
 
-int _tmain()
+int wmain(int argc, WCHAR** argv, WCHAR** envp)
 {
+	UNREFERENCED_PARAMETER(envp);
+	UNREFERENCED_PARAMETER(argc);
 	NTSTATUS status;
 	UNICODE_STRING domain;
 	UNICODE_STRING account;
@@ -226,7 +241,6 @@ int _tmain()
 	PSID pSidLocal = NULL;
 	HANDLE hToken = INVALID_HANDLE_VALUE;
 	HANDLE hPriToken = INVALID_HANDLE_VALUE;
-	LSA_SID_NAME_MAPPING_OPERATION_ERROR result;
 	SID_IDENTIFIER_AUTHORITY localAuthority = SECURITY_LOCAL_SID_AUTHORITY;
 	SID_IDENTIFIER_AUTHORITY ntAuthority = SECURITY_NT_AUTHORITY;
 	const LPCWSTR cszCmdLine = L"\"C:\\Windows\\system32\\cmd.exe\"";
@@ -234,83 +248,49 @@ int _tmain()
 	size_t dwCmdLineLen;
 	PROCESS_INFORMATION pi = {0};
 	STARTUPINFO si = {0};
+	DWORD dwRid;
 
-	//i believe it is funny
-	InitUnicodeString(&domain, L"😎😎😎😎");
-	InitUnicodeString(&account, L"😈😈😈😈");
+	srand((unsigned)GetTickCount64());
+	dwRid = (DWORD)rand(); //NOLINT - limited randomness is ok here
+
+	if (argc > 2)
+	{
+		RtlInitUnicodeString(&domain, argv[1]);
+		RtlInitUnicodeString(&account, argv[2]);
+	}
+	else
+	{
+		//i believe it is funny
+		RtlInitUnicodeString(&domain, L"😎😎😎😎");
+		RtlInitUnicodeString(&account, L"😈😈😈😈");
+	}
 
 	//let's setup 3 SIDs needed
-	AllocateAndInitializeSid(
-		&ntAuthority,
-		1,
-		BASE_RID,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		&pSidDomain
-	);
-	_tprintf(TEXT("AllocateAndInitializeSid(domain) returned %ld\r\n"), GetLastError());
+	AllocateAndInitializeSid(&ntAuthority, 1, BASE_RID, 0, 0, 0, 0, 0, 0, 0, &pSidDomain);
+	wprintf(L"AllocateAndInitializeSid(domain) returned %ld\r\n", GetLastError());
 
-	AllocateAndInitializeSid(
-		&ntAuthority,
-		2,
-		BASE_RID,
-		1,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		&pSidUser
-	);
-	_tprintf(TEXT("AllocateAndInitializeSid(account) returned %ld\r\n"), GetLastError());
+	AllocateAndInitializeSid(&ntAuthority, 2, BASE_RID, dwRid, 0, 0, 0, 0, 0, 0, &pSidUser);
+	wprintf(L"AllocateAndInitializeSid(account) returned %ld\r\n", GetLastError());
 
-	AllocateAndInitializeSid(
-		&localAuthority,
-		1,
-		SECURITY_LOCAL_RID,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		&pSidLocal
-	);
-	_tprintf(TEXT("AllocateAndInitializeSid(pSidLocal) returned %ld\r\n"), GetLastError());
+	AllocateAndInitializeSid(&localAuthority, 1, SECURITY_LOCAL_RID, 0, 0, 0, 0, 0, 0, 0, &pSidLocal);
+	wprintf(L"AllocateAndInitializeSid(pSidLocal) returned %ld\r\n", GetLastError());
 
 	// let's map domain
-	status = AddSidMapping(
-		&domain,
-		NULL,
-		pSidDomain,
-		&result
-	);
-	_tprintf(TEXT("AddSidMapping(domain) returned %ld\r\n"), status);
+	status = AddSidMapping(&domain, NULL, pSidDomain);
+	wprintf(L"AddSidMapping(domain) returned %ld\r\n", status);
 
 	//let's handle lack of SeTcb explicitly, as it will be the most common error when playing with the code
 	if (STATUS_PRIVILEGE_NOT_HELD == status)
 	{
-		_tprintf(TEXT("The code will not work without SeTcbPrivilege. Exiting.\r\n"));
-		exit(STATUS_PRIVILEGE_NOT_HELD);
+		wprintf(L"The code will not work without SeTcbPrivilege. Exiting.\r\n");
+		return STATUS_PRIVILEGE_NOT_HELD;
 	}
 
 	//let's map account within the domain mapped above
-	status = AddSidMapping(
-		&domain,
-		&account,
-		pSidUser,
-		&result
-	);
-	_tprintf(TEXT("AddSidMapping(account) returned %ld\r\n"), status);
-	
-	//let's create impersonation token for the mapped domain\account
+	status = AddSidMapping(&domain, &account, pSidUser);
+	wprintf(L"AddSidMapping(account) returned %ld\r\n", status);
+
+	//let's create impersonation token for the mapped domain \ account
 	LogonUserExExW(
 		account.Buffer,
 		domain.Buffer,
@@ -322,52 +302,35 @@ int _tmain()
 		NULL,
 		NULL,
 		NULL,
-		NULL
-	);
-	_tprintf(TEXT("LogonUserExExW() returned %ld\r\n"), GetLastError());
+		NULL);
+	wprintf(L"LogonUserExExW() returned %ld\r\n", GetLastError());
 
 	//let's duplicate token to make it primary to make call CreateProcessAsUserW() possible
-	DuplicateTokenEx(
-		hToken,
-		MAXIMUM_ALLOWED,
-		NULL,
-		SecurityImpersonation,
-		TokenPrimary,
-		&hPriToken
-	);
-	_tprintf(TEXT("DuplicateTokenEx() returned %ld\r\n"), GetLastError());
+	DuplicateTokenEx(hToken, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenPrimary, &hPriToken);
+	wprintf(L"DuplicateTokenEx() returned %ld\r\n", GetLastError());
 
 	//mess required by CreateProcessAsUserW()
 	dwCmdLineLen = wcslen(cszCmdLine) + 1;
 	lpCmdLine = (WCHAR*)LocalAlloc(LPTR, dwCmdLineLen * sizeof(WCHAR));
 	if (!lpCmdLine)
 	{
-		_tprintf(TEXT("Not enough memory.\r\n"));
-		exit(ERROR_NOT_ENOUGH_MEMORY);
+		wprintf(L"Not enough memory.\r\n");
+		return ERROR_NOT_ENOUGH_MEMORY;
 	}
-	(VOID)StringCchCopy(lpCmdLine, dwCmdLineLen, cszCmdLine);
+	memcpy_s(lpCmdLine, LocalSize(lpCmdLine), cszCmdLine, (wcslen(cszCmdLine) * sizeof(WCHAR)));
 
 	si.cb = sizeof(si);
 	si.lpDesktop = L"Winsta0\\Default";
 
 	//create process with the token duplicated above
-	_tprintf(TEXT("CreateProcessAsUserW() ... "));
-	CreateProcessAsUserW(
-		hPriToken,
-		NULL,
-		lpCmdLine,
-		NULL,
-		NULL,
-		FALSE,
-		0,
-		NULL,
-		NULL,
-		&si,
-		&pi
-	);
-	_tprintf(TEXT("returned %ld\r\n\r\n"), GetLastError());
+	wprintf(L"CreateProcessAsUserW() ... ");
+	CreateProcessAsUserW(hPriToken, NULL, lpCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+	wprintf(L"returned %ld\r\n\r\n", GetLastError());
 
 	WaitForSingleObject(pi.hProcess, INFINITE);
 	CloseHandle(pi.hProcess);
 	CloseHandle(pi.hThread);
+
+	status = RemoveSidMapping(&domain, &account);
+	wprintf(L"RemoveSidMapping() returned %ld\r\n", status);
 }
