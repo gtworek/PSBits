23bde5ec54ad8a290f1ff8c664d9239822911689
Grzegorz Tworek <gtworek@users.noreply.github.com>
2023-07-25 08:38:33 +0200

    Create Wipe-RecentDocs.ps1

---
+++ DFIR/Wipe-RecentDocs.ps1
@@ -0,0 +1,14 @@
+// https://learn.microsoft.com/en-us/windows/win32/api/shlobj_core/nf-shlobj_core-shaddtorecentdocs
+$TypeDef = @"
+    using System;
+    using System.Runtime.InteropServices;
+    public static class Shell32
+	{
+		[DllImport("Shell32.dll", SetLastError=true)]
+		public static extern void SHAddToRecentDocs(
+			uint uFlags,
+            IntPtr pv);
+	}
+"@
+Add-Type -TypeDefinition $TypeDef
+[Shell32]::SHAddToRecentDocs(0,[IntPtr]::Zero)
